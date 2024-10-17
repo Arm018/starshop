b0388e2ed83ae3a54d72fb043f3ab7baa593deec
Arm018 <arm0182626@gmail.com>
2024-10-17 15:04:07 +0400

    Added the service, ApiStarshipsController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,12 +12,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function homepage(): Response
+    public function homepage(StarshipRepository $repository): Response
     {
-        $countStarship = 123;
+        $ships = $repository->getAll();
+        $myShip = $ships[array_rand($ships)];
 
         return $this->render('home/home.html.twig', [
-            'count_starship' => $countStarship,
+            'ships' => $ships,
+            'myShip' => $myShip,
         ]);
     }
 }
